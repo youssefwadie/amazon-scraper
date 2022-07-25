c9a56c027e0d9f0c519433e2d9e8d065e6be70bf
Youssef Wadie <youssefwadie14@gmail.com>
2022-07-25 15:00:49 +0200

    code upload

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# amazon-scraper
-Web scraper for amazon products
+# Amazon Scraper
+
+ِWeb scraper for [Amazon](https://www.amazon.com) products with Scrapy
+
+<p>
+    Scrape <a href="https://www.amazon.com">amazon</a> products details, output them in json format
+    <strong>amazon may block your IP, or throws at you 503 service unavailable error. So, consider using a proxy for better results</strong>
+</p>
+
+# Installation
+
+1. Install scrapy with [pip](#pip_install), [conda](#conda_install) or [micromamba](#mamba_install)
+2. `git clone https://github.com/youssefwadie/amazon-scraper.git`
+3. `cd amazon-scraper`
+4. Edit the [query list](/amazon_scrapper/spiders/amazon.py#L6)
+5. `scrapy crawl amazon -o outfile.json`
+
+```bash
+scrapy crawl amazon -o outfile.json
+```
+
+pip installation <a name="pip_install"></a>
+MacOS/Linux
+
+```bash
+python3 -m pip install --user virtualenv
+python3 -m venv test
+source test/bin/activate
+pip install scrapy
+```
+
+Windows
+
+```bash
+py -m pip install --user virtualenv
+py -m venv test
+.\test\Scripts\activate
+pip install scrapy
+```
+
+---
+conda installation <a name="conda_install"></a>
+
+```bash
+conda create -n test -c conda-forge python scrapy
+conda activate test
+```
+
+---
+micromamba installation<a name="mamba_install"></a>
+
+```bash
+micromamba create -n test -c conda-forge python scrapy
+micromamba activate test
+```
+
+---
+Sample output
+
+```json
+ {
+  "asin": "B08NWCT6SK",
+  "title": "Moto G Stylus | 2021 | 2-Day Battery | Unlocked | Made for US by Motorola | 4/128GB | 48MP Camera | White",
+  "main_image": "https://m.media-amazon.com/images/I/414X4yF8zcL._AC_.jpg",
+  "price": "199",
+  "sizes": [
+    "128GB",
+    "256GB"
+  ],
+  "colors": [
+    "Black",
+    "Metallic Rose",
+    "Twilight Blue",
+    "White",
+    "Emerald",
+    "Seafoam Green"
+  ],
+  "features": [
+    "Unlocked for the freedom to choose your carrier. Compatible with AT&T, Sprint, T-Mobile, and Verizon networks. Sim card not included. Customers may need to contact Sprint for activation on Sprints network.",
+    "Built-in stylus. Retouch photos, jot notes, sketch artwork, and control a growing number of games and all apps with pinpoint precision.",
+    "48 MP quad camera system. Showcase your creativity from every perspective, from ultra-wide angle shots to detailed close-ups and everything in between.",
+    "Faster, smoother performance. Feel the instant response with every touch and tap of the stylus thanks to the Qualcomm Snapdragon 678 processor.",
+    "128 GB of storage. Carry more photos, songs, games, and movies and never give storage a second thought.",
+    "6.8\" Max Vision FHD, display. Watch your photos, movies, and video chats come to life on the biggest moto g display ever.",
+    "Up to 2 days battery life. Go longer on a single charge with a 4000 mAh battery.",
+    "Operating System: Android 10",
+    "Wi-Fi Hotspot Ready: Moto G Power (2021) offers Wi-fi hotspot connectivity in 2.4 GHz , 5 GHz for the best experience.",
+    "In-box: Moto G Stylus (2021), 10W rapid Charger, USB Cable, Guides, SIM tool"
+  ],
+  "list_price": "$299.99",
+  "images": [
+    "https://m.media-amazon.com/images/I/41kNnMYElKL._AC_US40_.jpg",
+    "https://m.media-amazon.com/images/I/61wo4NtXXWL.SS40_BG85,85,85_BR-120_PKdp-play-icon-overlay__.jpg",
+    "https://m.media-amazon.com/images/I/41KLxfKjR2L._AC_US40_.jpg",
+    "https://m.media-amazon.com/images/I/31M4u0EvKoL._AC_US40_.jpg",
+    "https://m.media-amazon.com/images/I/414X4yF8zcL._AC_US40_.jpg",
+    "https://m.media-amazon.com/images/I/31wy+smU1uL._AC_US40_.jpg",
+    "https://m.media-amazon.com/images/I/41dWTCmUQGL._AC_US40_.jpg"
+  ],
+  "dimensions": {
+    "length": 6.6,
+    "width": 1.84,
+    "height": 3.09
+  },
+  "weight": 13.8
+}
+```
---
+++ amazon_scraper/spiders/amazon.py
@@ -0,0 +1,104 @@
+import scrapy
+from urllib.parse import urlencode, urljoin
+import re
+import json
+
+queries = ["gaming laptop",
+           "smart tv"]
+
+
+def to_ascii(string: str) -> str:
+    return string.encode('ascii', 'ignore').decode('ascii').strip()
+
+
+class AmazonSpider(scrapy.Spider):
+    name = 'amazon'
+    headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'dnt': '1',
+        'referer': 'https://www.amazon.com/',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    def start_requests(self):
+        for query in queries:
+            url = 'https://www.amazon.com/s?' + urlencode({'k': query})
+            yield scrapy.Request(url=url, callback=self.parse_keyword_response)
+
+    def parse_keyword_response(self, response):
+        products = response.xpath('//*[@data-asin]')
+
+        for product in products:
+            asin = product.xpath('@data-asin').extract_first()
+            product_url = f'https://www.amazon.com/dp/{asin}'
+            yield scrapy.Request(url=product_url, callback=self.parse_product_page,
+                                 meta={'asin': asin})
+
+        next_page = response.xpath('//li[@class="a-last"]/a/@href').extract_first()
+        if next_page:
+            url = urljoin("https://www.amazon.com", next_page)
+            yield scrapy.Request(url=url, callback=self.parse_keyword_response)
+
+    def parse_product_page(self, response):
+        asin = response.meta['asin']
+        title = response.xpath('//*[@id="productTitle"]/text()').extract_first().strip()
+        image = re.search('"large":"(.*?)"', response.text).groups()[0]
+        price = response.xpath(
+            '//*[(@id = "corePriceDisplay_desktop_feature_div")]//*[contains(concat( " ", @class, " " ), concat( " ", "a-price-whole", " " ))]/text()').extract_first()
+        list_price = response.xpath(
+            '//*[@id="corePriceDisplay_desktop_feature_div"]/div[2]/span/span[1]/span/span[1]/text()').extract_first()
+
+        temp = response.xpath('//*[@id="twister"]')
+
+        sizes = []
+        colors = []
+        if temp:
+            s = re.search('"variationValues" : ({.*})', response.text).groups()[0]
+            json_acceptable = s.replace("'", "\"")
+            di = json.loads(json_acceptable)
+            sizes = di.get('size_name', [])
+            colors = di.get('color_name', [])
+
+        details_table = response.xpath('//*[@id="productDetails_techSpec_section_2"]/tr')
+        weight = None
+        dimensions = None
+
+        for detail in details_table:
+            entry_details_text = detail.xpath(
+                ".//*[contains(@class, 'prodDetSectionEntry')]/text()").extract_first()
+            if entry_details_text is None:
+                continue
+            if 'Weight' in entry_details_text:
+                weight = detail.xpath(".//*[contains(@class, 'prodDetAttrValue')]/text()").extract_first()
+            elif 'Dimensions' in entry_details_text:
+                dimensions = detail.xpath(".//*[contains(@class, 'prodDetAttrValue')]/text()").extract_first()
+
+        dimensions_json = {}
+        if dimensions is not None:
+            dimensions = to_ascii(dimensions).split('x')
+            dimensions_json = {
+                'length': dimensions[0].strip(),
+                'width': dimensions[1].strip(),
+                'height': dimensions[2].split('inches')[0].strip()
+            }
+
+        if weight is not None:
+            weight = float(''.join(filter(lambda ch: ch.isdigit() or ch == '.', weight.strip())))
+
+        images_div = response.xpath('//*[@id="altImages"]//img/@src').extract()
+        images = set(filter(lambda img: not re.match('.*\\.gif', img), images_div))
+
+        features = response.xpath('//*[@id="feature-bullets"]//li/span/text()').extract()
+        if features is not None:
+            features = list(map(lambda feature: to_ascii(str(feature)),
+                                filter(lambda feature: feature.strip(), features)))
+
+        yield {'asin': asin, 'title': title, 'main_image': image,
+               'price': price, 'sizes': sizes, 'colors': colors, 'features': features,
+               'list_price': list_price, 'images': images, 'dimensions': dimensions_json, 'weight': weight}
